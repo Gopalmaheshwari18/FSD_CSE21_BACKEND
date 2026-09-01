6c60887a124fc9c34811ffa1d4862f3389a96c44
Gopal Maheshwari <gopalbirla181@gmail.com>
2026-09-01 15:06:29 +0530

    REST API

---
+++ REST API/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+const app = express();
+let users = [
+    {id:1,name:"Gopal",email:'gopalbirla181@gmail.com'},
+    {id:2,name:"Devu",email:'devubirla181@gmail.com'},
+];
+
+// GET: fetch all users data 
+app.get('/users',(req,res)=>{
+    res.json(users);
+});
+
+// POST: create a new user 
+app.post('/users',(req,res)=>{
+    let user ={
+        id:users.length+1,
+        name:req.body.name,
+        email:req.body.email
+    };
+        users.push(user);
+        res.json(user);
+    
+});
+app.listen(8000,()=>{
+    console.log("Server is running on http://localhost:8000");
+});
