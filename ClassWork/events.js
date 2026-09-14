4cd517e7343ff9b2db5e1ca889f65bf7c20032b0
Gopal Maheshwari <gopalbirla181@gmail.com>
2026-09-14 09:36:00 +0530

    ClassWork

---
+++ ClassWork/events.js
@@ -0,0 +1,7 @@
+//Events Module in jS
+// Event class uses  
+// on() : register event as listener  
+//emit() : trigger event (create and fire both happening due to this)
+
+const EventEmitter = require('events');
+const event = new EventEmitter();
